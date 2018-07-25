112167de288a5021ccbcfd21f45c90e661d7c737
42 Electronics <38914219+42electronics@users.noreply.github.com>
2018-07-25 11:42:39 -0400

    Create write.py
    Include write.py with repo for cloning

---
+++ write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+
+reader = SimpleMFRC522.SimpleMFRC522()
+
+try:
+        text = input('Text value to write:')
+        print("Place tag near reader")
+        reader.write(text)
+        print('Tag written')
+finally:
+        GPIO.cleanup()
